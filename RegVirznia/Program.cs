985426557b7c14674732ca97b733d0959c307d66
Alex <sashafriend2000@mail.ru>
2017-11-02 22:32:38 +0300

    Чутка красивее сделал

--- RegVirznia/Program.cs
+++ RegVirznia/Program.cs
@@ -15,12 +15,8 @@ namespace RegVirznia
             Regex regex = new Regex(@"(0[1-9]|1[12])\.(0[1-9]|[12][0-9]|3[01])\.([0-9]{4})");
             string text = File.ReadAllText("file.txt");
             foreach (Match match in regex.Matches(text))
-            {
                 if (match.Length != 0)
-                {
                     write.WriteLine(match.ToString());
-                }
-            }
             write.Close();
         }
     }
