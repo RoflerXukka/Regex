c21752147b5e487ba5839d2fd2424a09e10e0cff
Alex <sashafriend2000@mail.ru>
2017-11-02 19:44:47 +0300

    Добавьте файлы проекта.

---
+++ RegVirznia/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RegVirznia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter write = new StreamWriter("file2.txt");
+            Regex regex = new Regex(@"(0[1-9]|1[12])\.(0[1-9]|[12][0-9]|3[01])\.(\d\d\d\d)");
+            string text = File.ReadAllText("file.txt");
+            foreach (Match match in regex.Matches(text))
+            {
+                if (match.Length != 0)
+                {
+                    write.WriteLine(match.ToString());
+                }
+            }
+            write.Close();
+        }
+    }
+}
